298c976d4e4219c8a9361432ee19ecfc3db50fff
Mateusz Łoskot <mateusz@loskot.net>
2015-12-03 23:43:02 +0100

    Add support for custom config_local.js

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 var config = {};
-
-// Define your own target URL
-config.url = 'http://google.com/search?'
+config.url = 'https://www.google.com?';
+        
+var fs = require('fs');
+if (fs.statSync("./config_local.js").isFile()) {    
+    var config_local = require('./config_local.js'); 
+    if (typeof config_local.url !== 'undefined') {
+        config.url = config_local.url;
+    }
+}
 
 module.exports = config;
