5bb235292deb1b52fde0e3cc742c4a066a582b52
Mateusz Łoskot <mateusz@loskot.net>
2015-12-03 23:45:20 +0100

    Add support for custom config_local.py

--- test-urllib.py
+++ test-urllib.py
@@ -1,4 +1,8 @@
-import config
+try:
+    import config_local as config
+except:
+    import config
+
 import urllib.request
 body = urllib.request.urlopen(config.url).read()
 print(body)
--- test-urllib3.py
+++ test-urllib3.py
@@ -1,4 +1,8 @@
-import config
+try:
+    import config_local as config
+except:
+    import config
+
 import urllib3
 http = urllib3.PoolManager()
 response = http.request('GET', config.url)
